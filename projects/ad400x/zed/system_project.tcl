c09a4f516515d8c379ed82dc53461761f06acd27
Istvan Csomortani <istvan.csomortani@analog.com>
2018-03-01 17:00:56 +0200

    ad400x: Explicitly state out the supported devices

--- projects/ad400x/zed/system_project.tcl
+++ projects/ad400x/zed/system_project.tcl
@@ -3,6 +3,15 @@ source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
+## The following HDL projects supports all the devices of EVAL-AD40XX-FMCZ:
+##
+##  AD4000/AD4001/AD4002/AD4003/AD4004/AD4005/AD4006/AD4007/AD4008/AD4010/AD4011/AD4020
+##
+## NOTE: Make sure that you set up your required ADC resolution and sampling rate
+##       in system_bd.tcl
+##
+## For more information see http://www.analog.com/en/products/analog-to-digital-converters/precision-adc-20msps/single-channel-ad-converters/ad4000.html
+
 adi_project_xilinx ad400x_zed
 
 adi_project_files ad400x_zed [list \
